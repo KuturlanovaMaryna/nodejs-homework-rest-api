54fb3ffe5986051c8b700428ec7008c77edf5c6f
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2024-01-19 18:42:55 +0200

    add schemas

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const emailRegexp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const subscriptionList = ["starter", "pro", "business"];
+
+const userShema = new mongoose.Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: subscriptionList,
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const subscriptionListSchema = Joi.object({
+  subscription: Joi.string()
+    .valid(...subscriptionList)
+    .required(),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+  subscriptionListSchema,
+};
+const User = mongoose.model("user", userShema);
+
+module.exports = { User, schemas };
