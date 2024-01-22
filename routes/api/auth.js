72e17fb91a748829a59cb4ed1ab163a81a7f9e8c
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2024-01-22 21:27:27 +0200

    fix exports

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,9 @@
 const express = require("express");
-
+const router = express.Router();
 const ctrl = require("../../controllers/auth");
 
 const { schemas } = require("../../models/user");
 const { validateBody } = require("../../middlewares/validateBody");
-
-const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
